705482ce7145f9f65dee8587c77af21663998ffd
Minihodari12 <152394434+Minihodari12@users.noreply.github.com>
2025-06-25 15:51:32 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -5,7 +5,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "HansTz&Ltx3kL4b#dYwDLOTGRlrj6yPfPN7XtzXb1WL7sG5Tw6_lSsstUCc",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
